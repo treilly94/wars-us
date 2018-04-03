b13f9d0a2e790c35f516630fd38a819cbc032339
treilly94 <treilly94@live.com>
2018-04-03 16:00:43 +0100

    made scrape all tables

--- scraping.R
+++ scraping.R
@@ -7,24 +7,27 @@ wiki_raw <- read_html("https://en.wikipedia.org/wiki/List_of_wars_involving_the_
 
 # Scrape wars
 wars <- wiki_raw %>% 
-  html_nodes(xpath = "/html/body/div[3]/div[3]/div[4]/div/table[2]") %>%
+  html_nodes("table.wikitable") %>%
   html_table()
 
-wars <- wars[[1]]
-
-# Extract names
+# Define patterns for regex
 name_pattern <- "([\\s\\w'-]+)\\n"
-wars$Name <- str_extract(wars$Conflict, pattern = name_pattern)
-
-# Extract dates 
 date_pattern <- "([\\d–]+)"
-wars$Dates <- str_extract(wars$Conflict, pattern = date_pattern)
 
-wars <- wars %>%
-  separate(Dates, c("Start", "End"), "–")
-
-# Drop extra columns
-wars <- wars[, c("Name", "Start", "End")]
-
-# Write to csv
-write.csv(wars, file = "wars.csv", row.names = FALSE)
+for (war in wars) {
+  
+  # Extract names
+  war$Name <- str_extract(war$Conflict, pattern = name_pattern)
+  
+  # Extract dates 
+  war$Dates <- str_extract(war$Conflict, pattern = date_pattern)
+  
+  war <- war %>%
+    separate(Dates, c("Start", "End"), "–")
+  
+  # Drop extra columns
+  war <- war[, c("Name", "Start", "End")]
+  
+  # Write to csv
+  write.table(war, file = "wars.csv", sep = ",", row.names = FALSE, append = TRUE)
+}
