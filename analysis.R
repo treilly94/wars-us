b9bebde63dd5860fa22263de8df60cf2299c69a9
treilly94 <treilly94@live.com>
2018-04-03 15:27:26 +0100

    hanged the colour of the plot

--- analysis.R
+++ analysis.R
@@ -8,8 +8,10 @@ wars$Name <- factor(wars$Name, levels = wars$Name[order(wars$Start)])
 
 # Create time plot
 g2 <- ggplot() +
-  geom_segment(data = wars, aes(x = Start, xend = End, y = Name, yend = Name)) +
+  geom_segment(data = wars, aes(x = Start, xend = End, y = Name, yend = Name, colour = Name), size = 2) +
+  scale_colour_brewer(palette = "Dark2") +
   xlab("Year") +
-  ylab("War")
+  ylab("War") +
+  theme(legend.position="none")
 
 g2
