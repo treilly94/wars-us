fe6c25498a322a64152bcce7978efee631c0b72a
treilly94 <treilly94@live.com>
2018-04-03 15:19:45 +0100

    Ordered graph

--- analysis.R
+++ analysis.R
@@ -3,3 +3,13 @@ library(ggplot2)
 # Read from csv 
 wars <- read.csv("wars.csv")
 
+# Order dataframe 
+wars$Name <- factor(wars$Name, levels = wars$Name[order(wars$Start)])
+
+# Create time plot
+g2 <- ggplot() +
+  geom_segment(data = wars, aes(x = Start, xend = End, y = Name, yend = Name)) +
+  xlab("Year") +
+  ylab("War")
+
+g2
