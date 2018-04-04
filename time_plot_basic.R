d637d0384127094bc02100859fd6b67f862e9693
treilly94 <treilly94@live.com>
2018-04-04 10:54:19 +0100

    improved and renamed the basic time plot

--- time_plot_basic.R
+++ time_plot_basic.R
@@ -6,12 +6,14 @@ wars <- read.csv("wars.csv")
 # Order dataframe 
 wars$Name <- factor(wars$Name, levels = wars$Name[order(wars$Start)])
 
-# Create time plot
-g2 <- ggplot() +
+# Create basic time plot
+timePlot <- ggplot() +
   geom_segment(data = wars, aes(x = Start, xend = End, y = Name, yend = Name), size = 2) +
   xlab("Year") +
-  ylab("War") +
-  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank())
+  ggtitle("US Wars Over Time") +
+  theme(axis.text.y = element_blank(), 
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank())
 
-g2
+timePlot
 
